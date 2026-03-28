d4335d9591652c34d81d62808b4a5971bcce765a
Darwin D Wu <darwin67@users.noreply.github.com>
2026-03-28 00:35:40 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Shōsai (書斎)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable."1.94.0".default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+
+            # tools
+            git-cliff
+
+            # deps
+            pkg-config
+            openssl
+
+            # LSP
+            rust-analyzer
+            nodePackages.yaml-language-server
+          ];
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      });
+}
